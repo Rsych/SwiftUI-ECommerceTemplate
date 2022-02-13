55719a427636fafd120ca0280cad5de79525ce31
Ryan <tamielryan@gmail.com>
2022-02-13 16:13:07 +0200

    Initial Commit

---
+++ SwiftUI-ECommerceTemplate/SwiftUI_ECommerceTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ECommerceTemplateApp.swift
+//  SwiftUI-ECommerceTemplate
+//
+//  Created by Ryan J. W. Kim on 2022/02/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ECommerceTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
